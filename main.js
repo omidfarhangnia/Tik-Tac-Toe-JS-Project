ba889bc579842bf671a72f6f7efcf498625b6410
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2022-10-05 22:39:23 +0330

    changing the classes for showing game

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-const NameContainer = document.querySelector(".name__container");
-const InputPlayerNumOne = document.getElementById("player__num__1__name");
-const InputPlayerNumTwo = document.getElementById("player__num__2__name");
 const StartGameBtn = document.querySelector(".start__game__btn");
-
 StartGameBtn.addEventListener("click" , StartGame);
+
 function StartGame(){
-    AddElementClass(NameContainer , "d-none");
+    const NameContainer = document.querySelector(".name__container");
+    const GameContainer = document.querySelector(".game__container")
+    const InputPlayerNumOne = document.getElementById("player__num__1__name");
+    const InputPlayerNumTwo = document.getElementById("player__num__2__name");
+    
+    if(InputPlayerNumOne.value === "" || InputPlayerNumTwo.value === ""){
+        alert("Please Fill Players Input Name");
+        return;
+    }
+    if(InputPlayerNumOne.value === InputPlayerNumTwo.value){
+        alert("You can't Choose Same Names");
+        return;
+    }
+
+    AddClassToElement(NameContainer , "hidden-style");
+    RemoveClassFromElement(GameContainer , "hidden-style");
 }
-function AddElementClass(element , className){
+function AddClassToElement(element , className){
     element.classList.add(className);
 }
-function RemoveElementClass(element , className){
+function RemoveClassFromElement(element , className){
     element.classList.Remove(className);
 }
