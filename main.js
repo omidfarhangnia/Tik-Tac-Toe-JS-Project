35d7a6f44370cc7579d98d6011d274f69c0cbfb6
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2022-10-06 14:35:37 +0330

    add some rules for background and text color

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ function StartGame(){
     const NameContainer = document.querySelector(".name__container");
     const GameContainer = document.querySelector(".game__container");
     const InputPlayerNumOne = document.getElementById("player__num__1__name");
+    const ColorPlayerNumOne = document.getElementById("PlayerOneColor");
     const InputPlayerNumTwo = document.getElementById("player__num__2__name");
+    const ColorPlayerNumTwo = document.getElementById("PlayerTwoColor");
+        
 
     if(InputPlayerNumOne.value === "" || InputPlayerNumTwo.value === ""){
         alert("Please Fill Players Input Name");
@@ -23,14 +26,34 @@ function StartGame(){
         alert("You can't Choose Same Names");
         return;
     }
-    FillUserData(InputPlayerNumOne , InputPlayerNumTwo)
+    FillUserData(InputPlayerNumOne , ColorPlayerNumOne , InputPlayerNumTwo , ColorPlayerNumTwo);
     AddClassToElement(NameContainer , "hidden-style");
     RemoveClassFromElement(GameContainer , "hidden-style");
 }
-function FillUserData(InputPlayerNumOne , InputPlayerNumTwo){
+function FillUserData(Player1Input , Player1Color , Player2Input , Player2Color){
     const UserDataPlayer1 = document.querySelector(".UserData--player1");
     const UserDataPlayer2 = document.querySelector(".UserData--player2");
 
-    UserDataPlayer1.textContent = InputPlayerNumOne.value;
-    UserDataPlayer2.textContent = InputPlayerNumTwo.value;
+    // این فانکشن چک میکند که اگر رنگ وارد شده روشن است رنگ متن را تغییر میدهد که دیده شود
+    let CheckTheColor = (hex , element) => {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        result = result ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16)
+        } : null;
+ 
+        if(result.r >= 170 && result.g >= 170 && result.b >= 170){
+            element.style.color = "#000"
+        }
+    }
+
+    UserDataPlayer1.textContent = Player1Input.value;
+    UserDataPlayer2.textContent = Player2Input.value;
+
+    UserDataPlayer1.style.background  = Player1Color.value;
+    UserDataPlayer2.style.background  = Player2Color.value;
+ 
+    CheckTheColor(Player1Color.value , UserDataPlayer1)
+    CheckTheColor(Player2Color.value , UserDataPlayer2)
 }
