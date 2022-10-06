b6845e87ad7827a62e29f4750bbba963685dd0e3
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2022-10-06 13:51:50 +0330

    making connection between usernames and user datas

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const StartGameBtn = document.querySelector(".start__game__btn");
 StartGameBtn.addEventListener("click" , StartGame);
 
+// فانکشنی برای اضافه کردن کلاس 
+function AddClassToElement(element , className){
+    element.classList.add(className);
+}
+// فانکشنی برای حذف کردن کلاس 
+function RemoveClassFromElement(element , className){
+    element.classList.remove(className);
+}
 function StartGame(){
     const NameContainer = document.querySelector(".name__container");
-    const GameContainer = document.querySelector(".game__container")
+    const GameContainer = document.querySelector(".game__container");
     const InputPlayerNumOne = document.getElementById("player__num__1__name");
     const InputPlayerNumTwo = document.getElementById("player__num__2__name");
-    
+
     if(InputPlayerNumOne.value === "" || InputPlayerNumTwo.value === ""){
         alert("Please Fill Players Input Name");
         return;
@@ -15,13 +23,14 @@ function StartGame(){
         alert("You can't Choose Same Names");
         return;
     }
-
+    FillUserData(InputPlayerNumOne , InputPlayerNumTwo)
     AddClassToElement(NameContainer , "hidden-style");
     RemoveClassFromElement(GameContainer , "hidden-style");
 }
-function AddClassToElement(element , className){
-    element.classList.add(className);
-}
-function RemoveClassFromElement(element , className){
-    element.classList.Remove(className);
+function FillUserData(InputPlayerNumOne , InputPlayerNumTwo){
+    const UserDataPlayer1 = document.querySelector(".UserData--player1");
+    const UserDataPlayer2 = document.querySelector(".UserData--player2");
+
+    UserDataPlayer1.textContent = InputPlayerNumOne.value;
+    UserDataPlayer2.textContent = InputPlayerNumTwo.value;
 }
